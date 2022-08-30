b0a9d047340e1349f7fb1014232c79127b581116
howard.chen <howardchen1617@gmail.com>
2022-08-30 17:31:44 +0800

    [update] layout fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import Footer from './components/layout/Footer';
-import Homepage from './pages/HomePage';
-import Header from './components/layout/Header';
 import { Routes, Route } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import Homepage from './pages/HomePage';
+import ExamplePage from './pages/ExamplePage';
 import './styles/style.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="example" element={<ExamplePage />} />
       </Routes>
       <Footer />
     </div>
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -7,42 +7,40 @@ import './_Header.scss';
 
 const Header = () => {
   return (
-    <div>
-      <div className="header_main d-flex justify-content-between mt-2">
-        <Link to="/" className="  d-block header_menu">
-          <div className="header_mobile_menu">選單一</div>
-          <img className=" header_dog-logo " src={logobody} alt="dog" />
+    <div className="header_main d-flex justify-content-between fixed-top bg-white">
+      <Link to="/" className="  d-block header_menu">
+        <div className="header_mobile_menu">選單一</div>
+        <img className=" header_dog-logo " src={logobody} alt="dog" />
+      </Link>
+      <div className="header_tittle d-flex justify-content-center align-items-center w-100 flex-shrink-1">
+        <ul className="header_container list-unstyled d-flex  mt-3">
+          <Link to="123" className="header_product mx-3">
+            住宿
+          </Link>
+          <Link to="/" className="header_product mx-3">
+            景點&玩樂
+          </Link>
+          <Link to="/" className="header_product mx-3">
+            餐廳
+          </Link>
+          <Link to="/" className="header_product mx-3">
+            寵物商品
+          </Link>
+          <Link to="/" className="header_product mx-3">
+            我的行程
+          </Link>
+          <Link to="/" className="header_product mx-3">
+            社群分享
+          </Link>
+        </ul>
+      </div>
+      <div className="  d-flex header_Icon align-items-center justify-content-end ">
+        <Link to="/" className="">
+          <AiOutlineShoppingCart />
         </Link>
-        <div className="header_tittle d-flex justify-content-center align-items-center w-100 flex-shrink-1">
-          <ul className="header_container list-unstyled d-flex  mt-3">
-            <Link to="123" className="header_product mx-3">
-              住宿
-            </Link>
-            <Link to="/" className="header_product mx-3">
-              景點&玩樂
-            </Link>
-            <Link to="/" className="header_product mx-3">
-              餐廳
-            </Link>
-            <Link to="/" className="header_product mx-3">
-              寵物商品
-            </Link>
-            <Link to="/" className="header_product mx-3">
-              我的行程
-            </Link>
-            <Link to="/" className="header_product mx-3">
-              社群分享
-            </Link>
-          </ul>
-        </div>
-        <div className="  d-flex header_Icon align-items-center justify-content-end ">
-          <Link to="/" className="">
-            <AiOutlineShoppingCart />
-          </Link>
-          <Link to="/" className="header_Icon_cart">
-            <AiOutlineUser />
-          </Link>
-        </div>
+        <Link to="/" className="header_Icon_cart">
+          <AiOutlineUser />
+        </Link>
       </div>
     </div>
   );
