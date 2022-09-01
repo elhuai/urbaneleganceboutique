a2feef1712e62f57e3e71fadd35d78d0f3f1807e
RainOuO <demon4920579@gmail.com>
2022-09-01 19:37:07 +0800

    [add] RWD Menu 0901 19:30 up

---
+++ src/components/layout/Menu/MenuLink.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+const MenuLink = () => {
+  return (
+    <>
+      <ul className="header_container list-unstyled d-flex  mt-3">
+        <NavLink to="path1" className="header_product mx-3">
+          住宿
+        </NavLink>
+        <NavLink to="path2" className="header_product mx-3">
+          景點&玩樂
+        </NavLink>
+        <NavLink to="path3" className="header_product mx-3">
+          餐廳
+        </NavLink>
+        <NavLink to="path4" className="header_product mx-3">
+          寵物商品
+        </NavLink>
+        <NavLink to="/travel" className="header_product mx-3">
+          我的行程
+        </NavLink>
+        <NavLink to="path6" className="header_product mx-3">
+          社群分享
+        </NavLink>
+      </ul>
+    </>
+  );
+};
+
+export default MenuLink;
