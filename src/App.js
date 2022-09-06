d48e54a740e993e00fcd452f20167a7b61183658
Nellielin <nellllen0402@gmail.com>
2022-09-06 13:26:38 +0800

    [ec-product-detail]update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './components/layout/Footer';
 import Homepage from './pages/HomePage';
+import ProductDetail from './pages/ProductDetail';
 import Header from './components/layout/Header';
 import { Routes, Route } from 'react-router-dom';
 // import './styles/style.scss';
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/ProductDetail" element={<ProductDetail />} />
       </Routes>
       <Footer />
     </div>
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logobody from '../../../images/logo_dog_body1.svg';
 import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 import { AiOutlineUser } from 'react-icons/ai';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import './_Header.scss';
@@ -9,39 +10,39 @@ const Header = () => {
   return (
     <div className="header_main_Body">
       <div className="header_main d-flex justify-content-between ">
-        <Link to="/" className="  d-block header_menu">
+        <NavLink to="/" className="  d-block header_menu">
           <div className="header_mobile_menu">選單一</div>
           <img className=" header_dog-logo " src={logobody} alt="dog" />
-        </Link>
+        </NavLink>
         <div className="header_tittle d-flex justify-content-center align-items-center w-100 flex-shrink-1">
           <ul className="header_container list-unstyled d-flex  mt-3">
-            <Link to="123" className="header_product mx-3">
+            <NavLink to="123" className="header_product mx-3">
               住宿
-            </Link>
-            <Link to="/" className="header_product mx-3">
+            </NavLink>
+            <NavLink to="/" className="header_product mx-3">
               景點&玩樂
-            </Link>
-            <Link to="/" className="header_product mx-3">
+            </NavLink>
+            <NavLink to="/" className="header_product mx-3">
               餐廳
-            </Link>
-            <Link to="/" className="header_product mx-3">
+            </NavLink>
+            <NavLink to="/ProductDetail" className="header_product mx-3" activeStyle={{ fontWeight: "bold", color: "red" }} exact={true}>
               寵物商品
-            </Link>
-            <Link to="/" className="header_product mx-3">
+            </NavLink>
+            <NavLink to="/" className="header_product mx-3">
               我的行程
-            </Link>
+            </NavLink>
             <Link to="/" className="header_product mx-3">
               社群分享
             </Link>
           </ul>
         </div>
         <div className="  d-flex header_Icon align-items-center justify-content-end ">
-          <Link to="/" className="">
+          <NavLink to="/" className="">
             <AiOutlineShoppingCart />
-          </Link>
-          <Link to="/" className="header_Icon_cart">
+          </NavLink>
+          <NavLink to="/" className="header_Icon_cart">
             <AiOutlineUser />
-          </Link>
+          </NavLink>
         </div>
       </div>
     </div>
