87c55eb596e873d7176108ce1274a8ca8cddc9ee
RainOuO <demon4920579@gmail.com>
2022-09-07 14:07:47 +0800

    up

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ExamplePage from './pages/ExamplePage';
 import Travel from './pages/Travel';
 import CommunityList from './pages/Community/';
 import AdminCenterPage from './pages/AdminCenter/AdminCenterPage';
+import Travelmap from './pages/Travel_map';
 import 'swiper/scss';
 import './styles/style.scss';
 
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="example" element={<ExamplePage />} />
         <Route path="/travel" element={<Travel />} />
+        <Route path="/Travel_map" element={<Travelmap />} />
         <Route path="/communitylist" element={<CommunityList />} />
         <Route path="/adminCenter" element={<AdminCenterPage />} />
       </Routes>
