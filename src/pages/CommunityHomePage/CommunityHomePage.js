774fabdfba96d28479a47dcc7c28b1553817093e
cyril-ke <cernd206@gmail.com>
2022-09-14 14:46:56 +0800

    [feat] communityHomePage Left&Right swiper desktop have one bug, RWD NOT BEGIN

--- src/pages/CommunityHomePage/CommunityHomePage.js
+++ src/pages/CommunityHomePage/CommunityHomePage.js
@@ -2,6 +2,7 @@ import './_CommunityHomePage.scss';
 import React from 'react';
 import SearchBar from '../../components/SearchBar/SearchList_search';
 import CommunitySwiperLeft from '../../components/CommunityHomePage/CommunitySwiperLeft/CommunitySwiperLeft';
+import CommunitySwiperRight from '../../components/CommunityHomePage/CommunitySwiperLeft/CommunitySwiperRight';
 
 
 const CommunityHomePage = () => {
@@ -9,6 +10,9 @@ const CommunityHomePage = () => {
     <>
       <SearchBar />
       <div className="comHome_container">
+        <CommunitySwiperRight />
+        <CommunitySwiperLeft />
+        <CommunitySwiperRight />
         <CommunitySwiperLeft />
       </div>
     </>
