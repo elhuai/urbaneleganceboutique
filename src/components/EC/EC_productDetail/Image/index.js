8235c50e0d392528c7a15fa853079d655aa588db
elhuai <el.huai97@gmail.com>
2022-09-23 03:25:55 +0800

    [EC]Detail Pages

--- src/components/EC/EC_productDetail/Image/index.js
+++ src/components/EC/EC_productDetail/Image/index.js
@@ -1,1 +1,1 @@
-export { default } from './ItemsPicture.js';
+export { default } from './ItemsImage.js';
--- src/pages/Ecommerce/ProductDetail/ProductDetail.js
+++ src/pages/Ecommerce/ProductDetail/ProductDetail.js
@@ -6,25 +6,26 @@ import './_ProductDetail.scss';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-import Guess_U_Like1 from '../../../images/Guess_U_Like1.png';
-import Guess_U_Like2 from '../../../images/Guess_U_Like2.png';
-
 // 加入購物車跳出視窗
 import { handleSuccess } from '../../../utils/handler/handleStatusCard';
-
+// 商品照片
+// import ItemImage from '../../../components/EC/EC_productDetail/Image';
 // 街景
-// import Streeview from '../../../components/EC/EC_productDetail/StreeView';
+// import Streeview from '../../../components/EC/EC_productDetail/StreetView';
+// 商品評價
+import Score from './../../../components/EC/EC_productDetail/Score';
 
 const ProductDetail = () => {
   const [productData, setProductData] = useState([]);
   const [tag, setTags] = useState([]);
   const [photo, setPhoto] = useState([]);
   const [recommend, setRecommend] = useState([]);
+  const [mainURL, setMainURL] = useState('');
 
   const location = useLocation();
   const urlSearchParams = new URLSearchParams(location.search);
   const productId = urlSearchParams.get('id');
-  const typeId = urlSearchParams.get('typeid');
+  // const typeId = urlSearchParams.get('typeid');
   // console.log('productId1111', productId);
 
   useEffect(() => {
@@ -39,6 +40,8 @@ const ProductDetail = () => {
       setTags(tag);
       const photos = result.data.photo;
       setPhoto(photos);
+      const mainURL = result.data.photo_path;
+      setMainURL(mainURL);
       setProductData(result.data);
 
       // 抓推薦 第四館商品資料
@@ -48,7 +51,6 @@ const ProductDetail = () => {
     fetchProductData();
   }, []);
   console.log('recommend', recommend);
-  const mainURL = productData.photo_path;
 
   return (
     <>
@@ -58,7 +60,9 @@ const ProductDetail = () => {
         <div className="productDetail mt-2">
           <div className="topRow">
             {/* 商品照區域 */}
-            <div className="imageColumn">
+            {/* <ItemImage /> */}
+
+            {/* <div className="imageColumn">
               <div className="mainImage">
                 <img
                   src={`http://localhost:3007${mainURL}/${productData.main_photo}`}
@@ -77,7 +81,8 @@ const ProductDetail = () => {
                   );
                 })}
               </div>
-            </div>
+            </div> */}
+            {/* 商品標題 */}
             <div className="productDesc">
               <div className="productContainer">
                 <h1 className="headText fw-bolder">{productData.name}</h1>
@@ -103,23 +108,25 @@ const ProductDetail = () => {
 
                 {/* 加購區 */}
                 <div className="mainAddSection">
-                  <p className="text-muted">+ 尚優惠的加購價</p>
+                  <p className="text-muted fw-bolder">+ 尚優惠的加購價</p>
                   <hr />
                   {/* 推薦商品 */}
                   {recommend.map((data, index) => {
                     return (
                       <div className="addSection d-flex flex-row mb-2">
-                        <div className="addSubSection d-flex align-items-center mb-0">
-                          <img
-                            className="addSubSection_img"
-                            // TODO 要記得換
-                            src={`http://localhost:3007${mainURL}/${productData.main_photo}`}
-                            alt="..."
-                          />
-                          <p className="addSubSection_name fw-bolder mb-0 ms-2">
-                            {data.name}
-                          </p>
-                        </div>
+                        <Link to={`/ec-productdetail?id=${data.id}`}>
+                          <div className="addSubSection d-flex align-items-center mb-0">
+                            <img
+                              className="addSubSection_img"
+                              // TODO 要記得換
+                              src={`http://localhost:3007${mainURL}/${productData.main_photo}`}
+                              alt="..."
+                            />
+                            <p className="addSubSection_name fw-bolder mb-0 ms-2">
+                              {data.name}
+                            </p>
+                          </div>
+                        </Link>
                         <div className="addCartSection d-flex flex-row">
                           <div className="addCostSection">
                             <p className="addCost text-decoration-line-through m-0">
@@ -161,53 +168,57 @@ const ProductDetail = () => {
               </div>
             </div>
           </div>
-          <hr />
-          <div className="bottomRow">
-            <div className="spec">
-              <div className="anchor">
-                <a href="#description">｜商品說明</a>
-                <a href="#toKnow">｜購買須知</a>
-                <a href="#howToUse">｜如何使用</a>
-                <a href="#comment">｜票券評價</a>
+          {/* <hr /> */}
+          <div className="descriptionSelection">
+            <div className="bottomRow">
+              <div className="spec">
+                <div className="anchor">
+                  <a href="#description">｜商品說明</a>
+                  <a href="#toKnow">｜購買須知</a>
+                  <a href="#howToUse">｜如何使用</a>
+                  <a href="#comment">｜票券評價</a>
+                </div>
+                <div className="anchor1"></div>
               </div>
-              <div className="anchor1"></div>
-            </div>
-            <div className="spec2">
-              <div id="description" className="description">
-                <h4>商品說明</h4>
-                <p>{productData.description}</p>
-                <img src={''} alt="" />
-              </div>
+              <div className="spec2">
+                <div id="description" className="description">
+                  <h4>商品說明</h4>
+                  <p>{productData.description}</p>
+                  <img src={''} alt="" />
+                </div>
 
-              <div id="toKnow" className="description">
-                <h4>購買須知</h4>
-                <p>
-                  •
-                  為提供顧客良好住宿體驗，不提供加人加價服務，請依適合人數選擇房型。
-                  <br />
-                  • 客房格局依實際入住安排為主。
-                  <br />
-                  •
-                  入住登記者須年滿18歲。未滿18歲之未成年旅客，須由家長陪同入住。
-                  <br />
-                  • 每間客房最多容納 1 名 0-12 歲的孩童，使用現有床鋪無法加床。
-                  <br />
-                </p>
+                <div id="toKnow" className="description">
+                  <h4>購買須知</h4>
+                  <p>
+                    •
+                    為提供顧客良好住宿體驗，不提供加人加價服務，請依適合人數選擇房型。
+                    <br />
+                    • 客房格局依實際入住安排為主。
+                    <br />
+                    •
+                    入住登記者須年滿18歲。未滿18歲之未成年旅客，須由家長陪同入住。
+                    <br />
+                    • 每間客房最多容納 1 名 0-12
+                    歲的孩童，使用現有床鋪無法加床。
+                    <br />
+                  </p>
+                </div>
+                <div id="howToUse" className="description">
+                  <h4>如何使用</h4>
+                  <p>
+                    • 憑證使用方式
+                    <br />
+                    現場請出示電子憑證與護照正本或身份證 text
+                    <br />
+                    • 憑證兌換期限
+                    <br />
+                    需要按照預訂日期及當天開放時間內兌換，逾期失效
+                    <br />
+                  </p>
+                </div>
+                {/* <Streeview /> */}
+                <Score />
               </div>
-              <div id="howToUse" className="description">
-                <h4>如何使用</h4>
-                <p>
-                  • 憑證使用方式
-                  <br />
-                  現場請出示電子憑證與護照正本或身份證 text
-                  <br />
-                  • 憑證兌換期限
-                  <br />
-                  需要按照預訂日期及當天開放時間內兌換，逾期失效
-                  <br />
-                </p>
-              </div>
-              {/* <Streeview /> */}
             </div>
           </div>
         </div>
