184bb419de7f1025f48ed6a2467318908f20e08d
elhuai <el.huai97@gmail.com>
2022-09-25 17:10:08 +0800

    [merge]devlop

--- src/components/EC/EC_homePage/EC_data/Category/RestaurantCategory.js
+++ src/components/EC/EC_homePage/EC_data/Category/RestaurantCategory.js
@@ -12,8 +12,12 @@ export const restaurantCategory = [
     img: require('../../../../../images/EC_HomePages_category/res_bao.png'),
   },
   {
+    name: '日式料理',
+    img: require('../../../../../images/EC_HomePages_category/res_japan.png'),
+  },
+  {
     name: '西式餐點',
-    img: require('../../../../../images/EC_HomePages_category/fun_burger.png'),
+    img: require('../../../../../images/EC_HomePages_category/res_burger.png'),
   },
   {
     name: '義式料理',
