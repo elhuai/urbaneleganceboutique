27ecc49e2e3e3610b49eb4fed3196ec9020593bf
Nellielin <nellllen0402@gmail.com>
2022-10-02 20:30:07 +0800

    feat(OrderDetail.js):showcart API update store_name

--- src/pages/Ecommerce/EC_checkout/subpages/OrderDetail.js
+++ src/pages/Ecommerce/EC_checkout/subpages/OrderDetail.js
@@ -72,6 +72,7 @@ function OrderDetail(props) {
         coupon_number: 8,
         coupon_name: '小確幸92折優惠',
         order_time: moment().format('YYYY-MM-DD HH:mm:ss'),
+        store_name:cartProductData.store_name,
       });
       // console.log('orderBuying', orderBuying);
     };
