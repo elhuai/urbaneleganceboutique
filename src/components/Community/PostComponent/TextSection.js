e391053c8c8d5eee893becf91d88f6d9dcdff05f
howard.chen <howardchen1617@gmail.com>
2022-10-03 14:35:53 +0800

    [update]

--- src/components/Community/PostComponent/TextSection.js
+++ src/components/Community/PostComponent/TextSection.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import parse from 'html-react-parser';
 
-import './TextSection.scss'
+import './TextSection.scss';
 
 function TextSection({ post }) {
   return (
     <>
-      <div className='postTextSection_main'>
+      <div className="postTextSection_main">
         <div className="article_section">{parse(post[0].content)}</div>
       </div>
     </>
